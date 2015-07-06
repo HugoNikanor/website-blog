8344d2f9967d689c044afc64ca4df744a6dada44
Hugo <hugo.hornquist@gmail.com>
2015-07-06 21:15:00 +0200

    Basic structure

---
+++ blog.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<meta charset="utf-8">
+<html>
+<link rel="stylesheet" href="./blog.css">
+<head>
+	<title>Blog</title>
+</head>
+<body>
+<div id="all">
+	<div id="top-bar">
+		<h1>top bar</h1>
+	</div>
+	<div id="nav-pane">
+		<a href="www.google.com">nav-pane</a>	
+	</div>
+	<div id="content">
+		<p>
+		<?php
+			echo("Content");
+		?>
+		</p>
+	</div>
+	<div id="footnote">
+		<p>footnote</p>
+	</div>
+</div>
+</body>
+</html>
