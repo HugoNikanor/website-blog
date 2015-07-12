3b033a4cb4786b165b60a69d0a0986cc17212837
Hugo <hugo.hornquist@gmail.com>
2015-07-12 20:23:20 +0200

    basic comments

--- blog.php
+++ blog.php
@@ -1,14 +1,14 @@
 <!DOCTYPE HTML>
-<meta charset="utf-8">
-<html>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <!--
 20150712 Hugo Hornquist
 
 Website for my personal blog
 -->
-<link rel="stylesheet" href="./blog.css">
+<html>
 <head>
 	<title>Blog</title>
+	<link rel="stylesheet" href="./blog.css">
 	<?php
 		error_reporting(E_ALL);
 		ini_set('display_errors', '1');
@@ -34,9 +34,13 @@ Website for my personal blog
 	<div id="nav-pane">
 		<?php
 			echo("<a href='./blog.php?id=first'>|<</a>");
-			echo("<a href='./blog.php?filename=" . $filename . "&id=prev'>Previous</a>");
-			echo("<a href='./blog.php?id=list'>List</a>");
-			echo("<a href='./blog.php?filename=" . $filename . "&id=next'>Next</a>");
+			echo("<a id='prev' href='./blog.php?filename=" . $filename . "&id=prev'>Previous</a>");
+			if($filename === 'list') {
+				echo("<a href='./blog.php'>Current entry</a>");
+			} else {
+				echo("<a href='./blog.php?id=list'>List</a>");
+			}
+			echo("<a id='next' href='./blog.php?filename=" . $filename . "&id=next'>Next</a>");
 			echo("<a href='./blog.php?id=latest'>>|</a>");
 		?>
 	</div>
@@ -67,7 +71,19 @@ Website for my personal blog
 		</p>
 	</div>
 	<div id="comment">
-		
+		<div id="disqus_thread"></div>
+		<script type="text/javascript">
+		    /* * * CONFIGURATION VARIABLES * * */
+		    var disqus_shortname = 'hugonikanor';
+		    
+		    /* * * DON'T EDIT BELOW THIS LINE * * */
+		    (function() {
+			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+		    })();
+		</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
 	</div>
 	<div id="footnote">
 		<a href="./blog.php?filename=about.md">About</a>
