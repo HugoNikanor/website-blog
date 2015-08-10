d3324492db25b49cbc33d2cfae4e87379ea4436a
Hugo <hugo.hornquist@gmail.com>
2015-08-10 18:51:30 +0200

    moved meta tag

--- blog.php
+++ blog.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <html>
 <head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <!--
 20150712 Hugo Hornquist
 
