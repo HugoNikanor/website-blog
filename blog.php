001073b9d6fd5a72c48598135f22e1d8f3ff2895
Hugo <hugo.hornquist@gmail.com>
2015-08-28 21:12:14 +0200

    actually actually fixed 'filename' problem

--- blog.php
+++ blog.php
@@ -25,17 +25,14 @@ Markup<?php /*and php*/ ?> for my personal blogg
 	<title>
 	<?php
 		echo("Hugos blogg");
-		if(!isset($_GET['filename'])) {
-			$fileExists = false;	
-		} else {
-			$fileExists = file_exists("./entries/" . $filename);
-			if($fileExists) {
-				$file_array = file("./entries/" . $filename);
-				if(substr($file_array[0], 0, 1) === "#") {
-					$title = $file_array[0];
-					unset($file_array[0]);
-					echo(" | " . substr($title, 1, strlen($title)));
-				}
+		$fileExists = file_exists("./entries/" . $filename);
+		if(empty($filename)) { $fileExists = false; }
+		if($fileExists) {
+			$file_array = file("./entries/" . $filename);
+			if(substr($file_array[0], 0, 1) === "#") {
+				$title = $file_array[0];
+				unset($file_array[0]);
+				echo(" | " . substr($title, 1, strlen($title)));
 			}
 		}
 	?>
