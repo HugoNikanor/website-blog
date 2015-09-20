c7fbd77f0caedce8d4d9bea745724cae50205263
Hugo <hugo.hornquist@gmail.com>
2015-09-20 18:08:11 +0200

    index on contentDisconnect: 8644230 WIP on contentDisconnect: 82c545a content tags started

--- content-strings.php
+++ content-strings.php
@@ -1,71 +1,29 @@
 <?php
 
-	$staticStrings = array(
-		"title" => "Hugos blogg",
-		"header" => "<u>Hugonikanors blogg<span style='letter-spacing:-0.65em'>?!</span></u>",
-		"subHeader" => "En blogg om datorer; spel, programmering & annat. Samt möjligen livet.",
+	$strings = parse_ini_file('content-strings.ini');
+	$urls    = parse_ini_file('urls.ini', true);
 
-		"navFirst"   => "|<",
-		"navPrev"    => "Föregående",
-		"navList"    => "Lista",
-		"navCurrent" => "Nuvarande inlägg",
-		"navNext"    => "Nästa",
-		"navLast"    => ">|", 
-
-		"author" => "Hugo Hornquist"
-	);
 	function getString($key) {
-		if(isset($staticStrings[$key]) {
-			return $staticStrings[$key];
+		$strings = parse_ini_file('content-strings.ini');
+		if(isset($strings[$key])) {
+			return $strings[$key];
 		}
 		else {
 			throw new Exception('getString > no such value');
 		}
 	}
 
-	$filepaths = array(
-		"rootDir" => "/blogEngine"
-
-	);
-	function getPath($key) {
-		if(isset($filePaths[$key]) {
-			return $filePaths[$key];
-		}
-		else {
-			throw new Exception('getPath > no such value');
-		}
-	}
-
-	# urlMode: fancy / raw
-	$urlMode = "fancy";
-
-	$urlsRaw = array(
-
-		"about"   => "./blog.php?filename=about.md",
-		"contact" => "./blog.php?filename=contact.md",
-		"legal"   => "./blog.php?filename=legal.md",
-		"qna"     => "./blog.php?filename=qna.md",
-	);
-
-	$urlFancy = array(
-
-		"about"   => "/blogg/about.md/",
-		"contact" => "/blogg/contact.md/",
-		"legal"   => "/blogg/legal.md/",
-		"qna"     => "/blogg/qna.md/",
-	);
-
 	function getUrl($key) {
-		if($urlMode === "fancy") {
-			if(isset($urlFancy[$key]) {
-				return $urlFancy[$key];
+		if($urls["urlStyle"] === "fancy") {
+			if(isset($urls["fancy"][$key])) {
+				return $urls["fancy"][$key];
 			}
 			else {
 				throw new Exception('getUrl > fancy > no such value');
 			}
-		} elseif( $urlMode === "raw" ) {
-			if(isset($urlRaw[$key]) {
-				return $urlRaw[$key];
+		} elseif( $urls["urlStyle"] === "raw" ) {
+			if(isset($urls["raw"][$key])) {
+				return $urls["raw"][$key];
 			}
 			else {
 				throw new Exception('getUrl > raw > no such value');
