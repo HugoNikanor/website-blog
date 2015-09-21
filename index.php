142c28f2e9b0c8a597da4ed96b7c5015df168717
Hugo <hugo.hornquist@gmail.com>
2015-09-21 17:57:20 +0200

    .gitignore changes

---
+++ index.php
@@ -0,0 +1,13 @@
+<!Doctype HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>Redirect</title>
+</head>
+<body>
+<?php
+	header( 'Location: blog.php' );	
+	die();
+?>
+</body>
+</html>
